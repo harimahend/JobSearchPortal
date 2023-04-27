9f9371ba24b5f2b034f498109c855c1184c1ecd1
harimahend <hariharan.mahendran49@gmail.com>
2023-04-27 15:37:34 +0530

    align change

--- src/Job.js
+++ src/Job.js
@@ -1,67 +1,60 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-const Job=(props)=>{
- 
-    const [Experiencemin, setExperiencemin] = useState("");
-    const[Experiencemax , setExperiencemax] =useState("");
-    const [Salarymin, setSalarymin] = useState("");
-    const[Salarymax ,setSalarymax] = useState("");
-    const [TotalEmployee, setTotalEmployee] = useState("");
-    
-  
-    const handleExperienceminChange = (event) => {
-      setExperiencemin(event.target.value);
-    };
-    const handleExperiencemaxChange = (event) => {
-      setExperiencemax(event.target.value);
-    };
-  
-    const handleSalaryminChange = (event) => {
-      setSalarymin(event.target.value);
-    };
-    const handleSalarymaxChange = (event) => {
-      setSalarymax(event.target.value);
-    };
-  
-    const handleTotalEmployeeChange = (event) => {
-      setTotalEmployee(event.target.value);
-    };
-  
-  
-  
-    const handleSaveClick = () => {
+const Job = (props) => {
+  const [Experiencemin, setExperiencemin] = useState("");
+  const [Experiencemax, setExperiencemax] = useState("");
+  const [Salarymin, setSalarymin] = useState("");
+  const [Salarymax, setSalarymax] = useState("");
+  const [TotalEmployee, setTotalEmployee] = useState("");
 
-      props.onSave();
-      const data = {
-        "jobTitle": localStorage.getItem('jobTitle'),
-        "companyName":localStorage.getItem('companyName'),
-        "industry": localStorage.getItem('industry'),
-        "location": localStorage.getItem('location'),
-        "remoteType":localStorage.getItem('remoteType'),
-        "Experiencemin":Experiencemin,
-        "Experiencemax":Experiencemax,
-        "Salarymin": Salarymin,
-        "Salarymax": Salarymax,
-       "TotalEmployee":TotalEmployee
+  const handleExperienceminChange = (event) => {
+    setExperiencemin(event.target.value);
+  };
+  const handleExperiencemaxChange = (event) => {
+    setExperiencemax(event.target.value);
+  };
 
-      };
-     fetch("https://643aacc290cd4ba563fee2b3.mockapi.io/fetchdata", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
+  const handleSalaryminChange = (event) => {
+    setSalarymin(event.target.value);
+  };
+  const handleSalarymaxChange = (event) => {
+    setSalarymax(event.target.value);
+  };
+
+  const handleTotalEmployeeChange = (event) => {
+    setTotalEmployee(event.target.value);
+  };
+
+  const handleSaveClick = () => {
+    props.onSave();
+    const data = {
+      jobTitle: localStorage.getItem("jobTitle"),
+      companyName: localStorage.getItem("companyName"),
+      industry: localStorage.getItem("industry"),
+      location: localStorage.getItem("location"),
+      remoteType: localStorage.getItem("remoteType"),
+      Experiencemin: Experiencemin,
+      Experiencemax: Experiencemax,
+      Salarymin: Salarymin,
+      Salarymax: Salarymax,
+      TotalEmployee: TotalEmployee,
+    };
+    fetch("https://643aacc290cd4ba563fee2b3.mockapi.io/fetchdata", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
       })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        }); 
-    
-  
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
     return (
       <form onSubmit={handleSaveClick}>
         <div>
@@ -74,7 +67,6 @@ const Job=(props)=>{
           />
         </div>
         <div>
-         
           <input
             id="experiencemax"
             type="text"
@@ -92,7 +84,6 @@ const Job=(props)=>{
           />
         </div>
         <div>
-         
           <input
             id="salarymax"
             type="text"
@@ -109,130 +100,163 @@ const Job=(props)=>{
             onChange={handleTotalEmployeeChange}
           />
         </div>
-       
+
         <button type="submit">Submit</button>
       </form>
     );
-  }
-  
+  };
 
-return(
-  
+  return (
     <div className=" mt-8 mr-8 mb-8 ml-8 w-[577px] h-[564px]">
       {/* Step 1 label */}
       <p className="float-right text-secondary font-medium">Step 2</p>
       <div className="mb-4">
-          <label className="text-left font-bold block mb-2" htmlFor="location">
-            Create a Job
-          </label>
-      <div className='w-6'>&nbsp;</div>
-    </div>
+        <label className="text-left font-bold block mb-2" htmlFor="location">
+          Create a Job
+        </label>
+        <div className="w-6">&nbsp;</div>
+      </div>
       {/* Form fields */}
-    
-    <div className="mb-4">
-      <label className="text-left font-medium block mb-2 " htmlFor="job-title">
-        Experience
-      </label>
-     <div className='h-1'>&nbsp;</div>
-    <div className="flex flex-wrap -mx-4">
 
-       <div className="  w-full md:w-1/2 px-4 mb-4">
-        
-       <input
-        className="appearance-none border rounded-md p-2 w-full border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
-        id="job-title"
-        type="text"
-        placeholder="Minimum"  value={Experiencemin} onChange={handleExperienceminChange}
-        
-        
-      />
-      </div>
-       <div className=" w-full md:w-1/2 px-4 mb-4">
-       <input
-        className="appearance-none border rounded-md p-2 w-full border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
-        id="job-title"
-        type="text"
-        placeholder="Maximum"  value={Experiencemax} onChange={handleExperiencemaxChange}     
-      />
-      </div>
-      </div>
-      <div className='w-6'>&nbsp;</div>
       <div className="mb-4">
-          <label className="text-left font-medium block mb-2" htmlFor="company-name">
+        <label
+          className="text-left font-medium block mb-2 "
+          htmlFor="job-title"
+        >
+          Experience
+        </label>
+        <div className="h-1">&nbsp;</div>
+        <div className="flex flex-wrap -mx-4">
+          <div className="  w-full md:w-1/2 px-4 mb-4">
+            <input
+              className="appearance-none border rounded-md p-2 w-full border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
+              id="job-title"
+              type="text"
+              placeholder="Minimum"
+              value={Experiencemin}
+              onChange={handleExperienceminChange}
+            />
+          </div>
+          <div className=" w-full md:w-1/2 px-4 mb-4">
+            <input
+              className="appearance-none border rounded-md p-2 w-full border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
+              id="job-title"
+              type="text"
+              placeholder="Maximum"
+              value={Experiencemax}
+              onChange={handleExperiencemaxChange}
+            />
+          </div>
+        </div>
+        <div className="w-6">&nbsp;</div>
+        <div className="mb-4">
+          <label
+            className="text-left font-medium block mb-2"
+            htmlFor="company-name"
+          >
             Salary
           </label>
-          <div className='h-1'>&nbsp;</div>
+          <div className="h-1">&nbsp;</div>
           <div className="flex flex-wrap -mx-4">
-          <div className=" w-full md:w-1/2 px-4 mb-4">
-          <input
-            className=" mr-6 appearance-none border border-secondary rounded w-full py-2 px-3  text-secondary leading-tight focus:outline-none focus:border-primary"
-            id="company-name"
-            type="text"
-            placeholder="Minimum"  value={Salarymin} onChange={handleSalaryminChange}
-          />
+            <div className=" w-full md:w-1/2 px-4 mb-4">
+              <input
+                className=" mr-6 appearance-none border border-secondary rounded w-full py-2 px-3  text-secondary leading-tight focus:outline-none focus:border-primary"
+                id="company-name"
+                type="text"
+                placeholder="Minimum"
+                value={Salarymin}
+                onChange={handleSalaryminChange}
+              />
+            </div>
+
+            <div className=" w-full md:w-1/2 px-4 mb-4">
+              <input
+                className="appearance-none border rounded-md p-2 w-full border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
+                id="job-title"
+                type="text"
+                placeholder="Maximum"
+                value={Salarymax}
+                onChange={handleSalarymaxChange}
+              />
+            </div>
           </div>
-         
-      <div className=" w-full md:w-1/2 px-4 mb-4">
-           <input
-        className="appearance-none border rounded-md p-2 w-full border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
-        id="job-title"
-        type="text"
-        placeholder="Maximum"  value={Salarymax} onChange={handleSalarymaxChange}
-          />
-      </div>
-         </div>
         </div>
-        <div className='w-6'>&nbsp;</div>
+        <div className="w-6">&nbsp;</div>
         <div className="mb-4">
-          <label className="text-left font-medium block mb-2" htmlFor="industry">
+          <label
+            className="text-left font-medium block mb-2"
+            htmlFor="industry"
+          >
             Total employee
           </label>
-          <div className='h-1'>&nbsp;</div>
+          <div className="h-1">&nbsp;</div>
           <input
             className="appearance-none border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
             id="industry"
             type="text"
-            placeholder="ex. 100"  value={TotalEmployee} onChange={handleTotalEmployeeChange}
-            
+            placeholder="ex. 100"
+            value={TotalEmployee}
+            onChange={handleTotalEmployeeChange}
           />
-
         </div>
-        <div className='w-6'>&nbsp;</div>
+        <div className="w-6">&nbsp;</div>
         <div className="mb-4">
-          <label className="text-left font-medium block mb-2" htmlFor="location">
+          <label
+            className="text-left font-medium block mb-2"
+            htmlFor="location"
+          >
             Apply type
           </label>
-          <div className='h-1'>&nbsp;</div>
+          <div className="h-1">&nbsp;</div>
           <div className="flex flex-wrap -mx-4">
-          <div className=" w-full md:w-1/2 px-4 mb-4">  
-<div class="flex items-center mb-4">
-    <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-    <label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Quick apply</label>
-</div>
-</div>
-<div className=" w-full md:w-1/2 px-4 mb-4">
-<div class="flex items-center">
-    <input checked id="default-radio-2" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-    <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">External apply</label>
-</div>
-
+            <div className=" w-full md:w-1/2 px-4 mb-4">
+              <div class="flex items-center mb-4">
+                <input
+                  id="default-radio-1"
+                  type="radio"
+                  value=""
+                  name="default-radio"
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  for="default-radio-1"
+                  class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Quick apply
+                </label>
+              </div>
+            </div>
+            <div className=" w-full md:w-1/2 px-4 mb-4">
+              <div class="flex items-center">
+                <input
+                  checked
+                  id="default-radio-2"
+                  type="radio"
+                  value=""
+                  name="default-radio"
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  for="default-radio-2"
+                  class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  External apply
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="float-right text-secondary font-medium">
+        <button
+          class=" 	text-secondary bg-[#1597E4] hover:bg-[#1597E4] text-white font-bold py-2 px-4 rounded"
+          onClick={handleSaveClick}
+        >
+          Save
+        </button>
+      </div>
     </div>
- </div>
-        </div>
-        </div>
-        <div className="float-right text-secondary font-medium">
-        <button class=" 	text-secondary bg-[#1597E4] hover:bg-[#1597E4] text-white font-bold py-2 px-4 rounded" 
-      onClick={handleSaveClick}>
-        Save
-     </button>
-    
-   </div>
-   </div>
-  
-);
-}
- 
-
-
+  );
+};
 
 export default Job;
--- src/Step1.js
+++ src/Step1.js
@@ -1,150 +1,161 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+const Step1 = (props) => {
+  const [jobTitle, setJobTitle] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [industry, setIndustry] = useState("");
+  const [location, setLocation] = useState("");
+  const [remoteType, setRemoteType] = useState("");
 
+  const handleJobTitleChange = (event) => {
+    setJobTitle(event.target.value);
+  };
 
-const Step1=(props)=>{
+  const handleCompanyNameChange = (event) => {
+    setCompanyName(event.target.value);
+  };
 
-        const [jobTitle, setJobTitle] = useState("");
-        const [companyName, setCompanyName] = useState("");
-        const [industry, setIndustry] = useState("");
-        const [location, setLocation] = useState("");
-        const [remoteType,setRemoteType]= useState("");
-    
-        const handleJobTitleChange = (event) => {
-            setJobTitle(event.target.value);
-        };
+  const handleIndustryChange = (event) => {
+    setIndustry(event.target.value);
+  };
 
-        const handleCompanyNameChange = (event) => {
-            setCompanyName(event.target.value);
-        };
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
+  const handleremoteTypeChange = (event) => {
+    setRemoteType(event.target.value);
+  };
+  const handleNextClick = () => {
+    props.onNext();
 
-        const handleIndustryChange = (event) => {
-            setIndustry(event.target.value);
-        };
+    localStorage.setItem("jobTitle", jobTitle);
+    localStorage.setItem("companyName", companyName);
+    localStorage.setItem("industry", industry);
+    localStorage.setItem("location", location);
+    localStorage.setItem("remoteType", remoteType);
+  };
 
-        const handleLocationChange = (event) => {
-            setLocation(event.target.value);
-        };
-        const handleremoteTypeChange =(event)=>{
-            setRemoteType(event.target.value);
-        }
-        const handleNextClick = () => {
-          props.onNext();
-        
-       
-          localStorage.setItem('jobTitle',jobTitle);
-          localStorage.setItem('companyName',companyName);
-          localStorage.setItem('industry',industry);
-          localStorage.setItem('location',location);
-          localStorage.setItem('remoteType',remoteType);
-               
-        };
-       
-   
-    return(
-      
-        <div className="   mt-8 mr-8 mb-8 ml-8 w-[577px] h-[564px]">
+  return (
+    <div className="   mt-8 mr-8 mb-8 ml-8 w-[577px] h-[564px]">
       {/* Step 1 label */}
       <p className=" float-right text-secondary font-medium">Step 1</p>
-      
-          <label className="text-left  font-[#212121] block mb-2" htmlFor="location">
-            Create a job
-          </label> 
+
+      <label
+        className="text-left  font-[#212121] block mb-2"
+        htmlFor="location"
+      >
+        Create a job
+      </label>
       {/* Form fields */}
-      <div className='w-6'>&nbsp;</div>
+      <div className="w-6">&nbsp;</div>
 
-        
-        
-          <label className="  text-left  font-medium block mb-2 " htmlFor="job-title">
-          <div class="relative">
-  
-  
-  Job Title
-  <span class="text-red-500">*</span>
-</div>
-
-               
-          </label>
-          <div className='h-1'>&nbsp;</div>
-          <input required 
-           
-            className="appearance-none border rounded-md p-2 w-full border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
-            id="job-title"
-            type="text"
-            placeholder="ex. UX UI Designer" value={jobTitle} onChange={handleJobTitleChange}
-          />
-       <div className='w-6'>&nbsp;</div>
-        <div className="  mb-4">
-          <label className="text-left font-medium block mb-2" htmlFor="company-name">
-            Company Name
-            <span class="text-red-500">*</span>
-          </label>
-          <div className='h-1'>&nbsp;</div>
-          <input required="true"
-            className="appearance-none border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
-            id="company-name"
-            type="text"
-            placeholder="ex. Google" value={companyName} onChange={handleCompanyNameChange}
-          />
+      <label
+        className="  text-left  font-medium block mb-2 "
+        htmlFor="job-title"
+      >
+        <div class="relative">
+          Job Title
+          <span class="text-red-500">*</span>
         </div>
-        <div className='w-6'>&nbsp;</div>
-        <div className="mb-4">
-          <label className="text-left font-medium block mb-2" htmlFor="industry">
-            Industry
-            <span class="text-red-500">*</span>
-          </label>
-          <div className='h-1'>&nbsp;</div>
-          <input
-            className="appearance-none border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
-            id="industry"
-            type="text"
-            placeholder="ex. Information Technology" value={industry} onChange={handleIndustryChange}
-          />
-        </div>
-        <div className='w-6'>&nbsp;</div>
-        <div className='h-1'>&nbsp;</div>
+      </label>
+      <div className="h-1">&nbsp;</div>
+      <input
+        required
+        className="appearance-none border rounded-md p-2 w-full border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
+        id="job-title"
+        type="text"
+        placeholder="ex. UX UI Designer"
+        value={jobTitle}
+        onChange={handleJobTitleChange}
+      />
+      <div className="w-6">&nbsp;</div>
+      <div className="  mb-4">
+        <label
+          className="text-left font-medium block mb-2"
+          htmlFor="company-name"
+        >
+          Company Name
+          <span class="text-red-500">*</span>
+        </label>
+        <div className="h-1">&nbsp;</div>
+        <input
+          required="true"
+          className="appearance-none border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
+          id="company-name"
+          type="text"
+          placeholder="ex. Google"
+          value={companyName}
+          onChange={handleCompanyNameChange}
+        />
+      </div>
+      <div className="w-6">&nbsp;</div>
+      <div className="mb-4">
+        <label className="text-left font-medium block mb-2" htmlFor="industry">
+          Industry
+          <span class="text-red-500">*</span>
+        </label>
+        <div className="h-1">&nbsp;</div>
+        <input
+          className="appearance-none border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
+          id="industry"
+          type="text"
+          placeholder="ex. Information Technology"
+          value={industry}
+          onChange={handleIndustryChange}
+        />
+      </div>
+      <div className="w-6">&nbsp;</div>
+      <div className="h-1">&nbsp;</div>
 
-        <div className="flex flex-wrap -mx-4">
+      <div className="flex flex-wrap -mx-4">
         <div className=" w-full md:w-1/2 px-4 mb-4">
-          <label className="text-left font-medium block mb-2" htmlFor="location">
+          <label
+            className="text-left font-medium block mb-2"
+            htmlFor="location"
+          >
             Location
           </label>
-          <div className='h-1'>&nbsp;</div>
+          <div className="h-1">&nbsp;</div>
           <input
             className="appearance-none border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
             id="location"
             type="text"
-            placeholder="ex. Chennai" value={location} onChange={handleLocationChange}
+            placeholder="ex. Chennai"
+            value={location}
+            onChange={handleLocationChange}
           />
         </div>
-       
+
         <div className="  w-full md:w-1/2 px-4 mb-4">
-          <label className="text-left font-medium block mb-2" htmlFor="remote-type">
+          <label
+            className="text-left font-medium block mb-2"
+            htmlFor="remote-type"
+          >
             Remote Type
           </label>
-          <div className='h-1'>&nbsp;</div>
+          <div className="h-1">&nbsp;</div>
           <input
             className="appearance-none border border-secondary rounded w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:border-primary"
             id="remote-type"
             type="text"
-            placeholder="Enter remote type" value={remoteType} onChange= {handleremoteTypeChange}
+            placeholder="Enter remote type"
+            value={remoteType}
+            onChange={handleremoteTypeChange}
           />
         </div>
-        </div>
-        {/* Next button */}
+      </div>
+      {/* Next button */}
 
       <div className="float-right text-secondary font-medium">
-     <button class="text-secondary  bg-[#1597E4] hover:bg-[#1597E4] text-white font-bold py-2 px-4 roundedfloat-right text-secondary font-medium" 
-      onClick={handleNextClick}>
-         Next
-     </button>
-     </div>
-     
-     
-     </div>
-     
-    );
-}
+        <button
+          class="text-secondary  bg-[#1597E4] hover:bg-[#1597E4] text-white font-bold py-2 px-4 roundedfloat-right text-secondary font-medium"
+          onClick={handleNextClick}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Step1;
-
